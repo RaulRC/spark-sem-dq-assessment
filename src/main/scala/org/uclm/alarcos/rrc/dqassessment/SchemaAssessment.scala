5b1ac1916c1c37eef3f9afee16be1162a3af9175
Raúl Reguillo Carmona <raul.reguillo@gmail.com>
2017-09-23 19:05:59 +0200

    [IMP] PoC with context-assessment

--- src/main/scala/org/uclm/alarcos/rrc/dqassessment/SchemaAssessment.scala
+++ src/main/scala/org/uclm/alarcos/rrc/dqassessment/SchemaAssessment.scala
@@ -4,18 +4,30 @@ import org.apache.spark.sql.SparkSession
 import org.uclm.alarcos.rrc.config.DQAssessmentConfiguration
 import org.uclm.alarcos.rrc.configDQ.SparkDQConfiguration
 import org.uclm.alarcos.rrc.dataquality.completeness.SchemaCompletenessMeasurement
+import org.uclm.alarcos.rrc.reasoning.Inference
+import org.apache.spark.sql.functions._
 
 
 /**
   * Created by raulreguillo on 6/09/17.
   */
-class SchemaAssessment(config: DQAssessmentConfiguration, sparkSession: SparkSession, inputFile: String) extends StepTrait with SchemaCompletenessMeasurement{
+class SchemaAssessment(config: DQAssessmentConfiguration, sparkSession: SparkSession, inputFile: String) extends StepTrait with SchemaCompletenessMeasurement with Inference{
   protected val processSparkSession: SparkSession = sparkSession
 
   def execute(): Unit = {
     val x: SparkDQConfiguration = null
     val graph = loadGraph(sparkSession, inputFile)
-    var result = getMeasurementSubgraph(graph.vertices, graph, config.properties)
+    def setLevels = udf((value: Double)=>{
+      if (value <=0.34)
+        "http://myontology.org/result#BAD"
+      else if (value <= 0.67)
+        "http://myontology.org/result#NORMAL"
+      else
+        "http://myontology.org/result#GOOD"
+    })
+
+    var result = applyRuleSet(getMeasurementSubgraph(graph.vertices, graph, config.properties),
+    "measurement", "contextualAssessment", setLevels)
     result.show(100, truncate = false)
   }
 }
