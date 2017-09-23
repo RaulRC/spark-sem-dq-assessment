14e92b4ce9cc57cd3b3cbac815e82218f8db1c9b
Raúl Reguillo Carmona <raul.reguillo@gmail.com>
2017-09-23 11:20:23 +0200

    [FIX] interlinking mess

--- src/main/scala/org/uclm/alarcos/rrc/dqassessment/SchemaAssessment.scala
+++ src/main/scala/org/uclm/alarcos/rrc/dqassessment/SchemaAssessment.scala
@@ -2,6 +2,7 @@ package org.uclm.alarcos.rrc.dqassessment
 
 import org.apache.spark.sql.SparkSession
 import org.uclm.alarcos.rrc.config.DQAssessmentConfiguration
+import org.uclm.alarcos.rrc.configDQ.SparkDQConfiguration
 import org.uclm.alarcos.rrc.dataquality.completeness.SchemaCompletenessMeasurement
 
 
@@ -12,6 +13,7 @@ class SchemaAssessment(config: DQAssessmentConfiguration, sparkSession: SparkSes
   protected val processSparkSession: SparkSession = sparkSession
 
   def execute(): Unit = {
+    val x: SparkDQConfiguration = null
     val graph = loadGraph(sparkSession, inputFile)
     var result = getMeasurementSubgraph(graph.vertices, graph, config.properties)
     result.show(100, truncate = false)
